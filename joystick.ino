574de500d39794b050d5988551da53cd70972b5f
Sámuel Kovács <sunikovacs@gmail.com>
2020-03-19 12:40:23 +0100

    first commit

---
+++ joystick.ino
@@ -0,0 +1,140 @@
+                                                                                            #include <usbhid.h>
+#include <hiduniversal.h>
+#include <usbhub.h>
+
+// Satisfy IDE, which only needs to see the include statment in the ino.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#endif
+#include <SPI.h>
+
+#include "hidjoystickrptparser.h"
+
+USB Usb;
+USBHub Hub(&Usb);
+HIDUniversal Hid(&Usb);
+JoystickEvents JoyEvents;
+JoystickReportParser Joy(&JoyEvents);
+
+
+
+
+
+//////////////////////CONFIGURATION///////////////////////////////
+#define CHANNEL_NUMBER 6  //set the number of chanels
+#define CHANNEL_DEFAULT_VALUE 1500  //set the default servo value
+#define FRAME_LENGTH 20000  //set the PPM frame length in microseconds (1ms = 1000µs)
+#define PULSE_LENGTH 400  //set the pulse length
+#define onState 0  //set polarity of the pulses: 1 is positive, 0 is negative
+#define sigPin 2  //set PPM signal output pin on the arduino
+//////////////////////////////////////////////////////////////////
+
+#define SWITCH_PIN 16
+#define CHANNEL_TO_MODIFY 11
+#define SWITCH_STEP 100
+
+byte previousSwitchValue;
+
+/*this array holds the servo values for the ppm signal
+ change theese values in your code (usually servo values move between 1000 and 2000)*/
+
+
+int currentChannelStep;
+
+
+
+
+void setup() {
+        Serial.begin(115200);
+#if !defined(__MIPSEL__)
+        while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
+        Serial.println("Start");
+
+        if (Usb.Init() == -1)
+                Serial.println("OSC did not start.");
+
+        delay(200);
+
+        if (!Hid.SetReportParser(0, &Joy))
+                ErrorMessage<uint8_t > (PSTR("SetReportParser"), 1);
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			previousSwitchValue = HIGH;
+  
+  //initiallize default ppm values
+  for(int i=0; i<CHANNEL_NUMBER; i++){
+    if (i == 2 || i == CHANNEL_TO_MODIFY) {
+      ppm[i] = 1000;
+    } else {
+      ppm[i]= CHANNEL_DEFAULT_VALUE;
+    }
+  }
+
+  pinMode(sigPin, OUTPUT);
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
+  digitalWrite(sigPin, !onState);  //set the PPM signal pin to the default state (off)
+  
+  cli();
+  TCCR1A = 0; // set entire TCCR1 register to 0
+  TCCR1B = 0;
+  
+  OCR1A = 100;  // compare match register, change this
+  TCCR1B |= (1 << WGM12);  // turn on CTC mode
+  TCCR1B |= (1 << CS11);  // 8 prescaler: 0,5 microseconds at 16mhz
+  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+  sei();
+
+  currentChannelStep = SWITCH_STEP;
+  
+}
+
+void loop() {
+        Usb.Task();
+}
+
+
+ISR(TIMER1_COMPA_vect){  //leave this alone
+  static volatile boolean state = true;
+  
+  TCNT1 = 0;
+
+  cli();
+  if (state) {  //start pulse
+    digitalWrite(sigPin, onState);
+    OCR1A = PULSE_LENGTH * 2;
+    state = false;
+  } else{  //end pulse and calculate when to start the next pulse
+    static volatile byte cur_chan_numb;
+    static volatile unsigned int calc_rest;
+  
+    digitalWrite(sigPin, !onState);
+    state = true;
+
+    if(cur_chan_numb >= CHANNEL_NUMBER){
+      cur_chan_numb = 0;
+      calc_rest = calc_rest + PULSE_LENGTH;// 
+      OCR1A = (FRAME_LENGTH - calc_rest) * 2;
+      calc_rest = 0;
+    }
+    else{
+      OCR1A = (ppm[cur_chan_numb] - PULSE_LENGTH) * 2;
+      calc_rest = calc_rest + ppm[cur_chan_numb];
+      cur_chan_numb++;
+    }     
+  }
+  sei();
+}
